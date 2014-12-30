1a73577e68a669168257b5f87683481a469e959a
leilaSaeed2014 <leilasaeed2013@gmail.com>
2014-12-30 23:16:16 +0200

    add comment

--- home_page.php
+++ home_page.php
@@ -5,25 +5,36 @@ $username = "LeilaSaeed";
 $password = "leila";
 $dbname = "E-Commerce";
 
+#connect database
 $conn = new mysqli($servername, $username, $password,$dbname);
 
-
+#check if click on log in
 if( isset($_POST["LogIn"]) ){
 
+	#check if set email
 	if(empty($_POST["email"])){
 		echo "<p> Enter email </p>" ;
 	}
-
+	
+	#check if set password
 	else if(empty($_POST["pass"])){
 		echo "<p> Enter password </p>";
 	}
 
 	else{
+		
+		#check if connect database true
 		if(! $conn ){
   		die('Could not connect: ' . mysql_error());
 		}	
+		
+		#select row of user
 		$sql = "SELECT email , password FROM customer";
-$id = "SELECT id FROM customer where email = '" . $_POST["email"] . "' and password = '". $_POST["pass"] . "'";		$result = mysqli_query($conn , $sql);
+		
+		#get id of user
+		$id = "SELECT id FROM customer where email = '" . $_POST["email"] . "' and password = '". $_POST["pass"] . "'";		$result = mysqli_query($conn , $sql);
+		
+		#check if this mail & pass already in sql 
 		if(! $result ){
 		  die('Could not get data: ' . mysql_error());
 		}
