517a596c8674fb051d5fbe4d9ddea077364a4f2f
leilaSaeed2014 <leilasaeed2013@gmail.com>
2014-12-31 20:29:37 +0200

    add home page && list application

--- home_page_customer/home_page.php
+++ home_page_customer/home_page.php
@@ -1,35 +1,46 @@
 <?php 
-
+session_start();
 $servername = "localhost";
 $username = "LeilaSaeed";
 $password = "leila";
 $dbname = "E-Commerce";
 
+#connect database
 $conn = new mysqli($servername, $username, $password,$dbname);
 
-
+#check if click on log in
 if( isset($_POST["LogIn"]) ){
 
+	#check if set email
 	if(empty($_POST["email"])){
 		echo "<p> Enter email </p>" ;
 	}
-
+	
+	#check if set password
 	else if(empty($_POST["pass"])){
 		echo "<p> Enter password </p>";
 	}
 
 	else{
+		
+		#check if connect database true
 		if(! $conn ){
-  		die('Could not connect: ' . mysql_error());
+  		echo " Could not connect ";
 		}	
+		
+		#select row of user
 		$sql = "SELECT email , password FROM customer";
-		$result = mysqli_query($conn , $sql);
-		if(! $query ){
-		  die('Could not get data: ' . mysql_error());
+		
+		#get id of user
+		$id = "SELECT id FROM customer where email = '" . $_POST["email"] . "' and password = '". $_POST["pass"] . "'";		$result = mysqli_query($conn , $sql);
+		
+		#check if this mail & pass already in sql 
+		if(! $result ){
+		  echo " Could not get data ";
 		}
+		$_SESSION['current_user'] = $id;
 		header("location: list_application.php");	
 	}
-
 }
 ?>
 
@@ -46,7 +57,7 @@ if( isset($_POST["LogIn"]) ){
  			<input name = "email" type = "text">
  			<br>
  			password <br>
- 			<input name = "pass" type = "text">
+ 			<input name = "pass" type = "password">
  			<br>
  			<br>
  			<input type = "submit" name = "LogIn" value = "LogIn" >  
--- home_page_customer/list_application.php
+++ home_page_customer/list_application.php
@@ -1,15 +1,23 @@
-<?php 
+<?php
+session_start(); 
 
 if( isset($_POST["start shopping"]) ){
-	header('location: productList.php');
+	header("location: productList.php");
 }
 else if( isset($_POST["track orders"]) ){
-	header('location: pageOfCustomer.php');
+	header("location: pageOfCustomer.php");
 	
+} 
+else if (isset($_POST["Your account"])){
+	header("location: UserInfoPage.php");
 }
-else if( isset($_POST["change your information"]) ){
-	header('location: pageOfCustomer.php');	
+else if( isset($_POST["cart"]) ){
+	header("location: cart.php");	
 }
+else if( isset($_POST["checkout"]) ){
+	header("location: checkout.php");	
+}
+
 
 ?>
 <html>
@@ -23,15 +31,28 @@ else if( isset($_POST["change your information"]) ){
  			<br>
  			<input type = "submit" name = "start shopping" value = "start shopping" >
  			<br>  
+ 		</form>
  			OR  
+ 		<form action = "list_application.php" method = "post">
  			<br>
  			<input type = "submit" name = "track orders" value = "track orders"> 
- 			<br>  
- 			OR  
- 			<br>
- 			<input type = "submit" name = "change your information" value = "change your information"> 
+ 			<br> 
  		</form>
-
+ 			OR
+ 		<form action = "list_application.php" method = "post"> 		 
+ 			<br>
+ 			<input type = "submit" name = "Your account" value = "Your account"> 
+ 		</form>
+ 			OR
+ 		<form action = "list_application.php" method = "post"> 		 
+ 			<br>
+ 			<input type = "submit" name = "cart" value = "cart"> 
+ 		</form>
+ 			OR
+ 		<form action = "list_application.php" method = "post"> 		 
+ 			<br>
+ 			<input type = "submit" name = "checkout" value = "checkout"> 
+ 		</form>
  	</body>
 
 </html>
